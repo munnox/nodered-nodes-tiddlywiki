57a8b9206cb656eceaa3780cf85035acde50bd1a
Robert Munnoch <munnox@hotmail.com>
2021-09-22 21:39:29 +0100

    Added a config node.

--- tiddlywiki.js
+++ tiddlywiki.js
@@ -1,4 +1,11 @@
 module.exports = function(RED) {
+    function RemoteServerNode(n) {
+        RED.nodes.createNode(this,n);
+        this.host = n.host;
+        this.port = n.port;
+    }
+    RED.nodes.registerType("tiddlywiki-server",RemoteServerNode);
+
     function LowerCaseNode(config) {
         RED.nodes.createNode(this,config);
         var node = this;
