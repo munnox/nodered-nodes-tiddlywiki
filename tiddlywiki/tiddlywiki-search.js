1774863b292fe167eafb7a930134776ee37c8d1d
Robert Munnoch <munnox@hotmail.com>
2021-09-25 12:53:25 +0100

    Cleaned up the node code and added some further metrics

--- tiddlywiki/tiddlywiki-search.js
+++ tiddlywiki/tiddlywiki-search.js
@@ -8,16 +8,12 @@ module.exports = function(RED) {
 
     function TiddlywikiSearchNode(config) {
         RED.nodes.createNode(this,config);
-        // this.server = {
-        //     host: "localhost",
-        //     port: 1880
-        // }
 
         // If the tiddlywiki server config node is defined then use it other wise fall back to local information
         if (config.tiddlywiki) {
             var server = RED.nodes.getNode(config.tiddlywiki)
             server.url = config.url || server.url;
-            // use the filter from the node faling back to the server default.
+            // Use the filter from the node faling back to the server default.
             server.filter = config.filter || server.filter;
         }
         else {
@@ -34,89 +30,64 @@ module.exports = function(RED) {
             // return;
         }
         var node = this;
-        // node.error(`Making node, Config:\n${JSON.stringify(config, null, 2)}`);
-        // node.error(`Node tiddlywiki server:\n${JSON.stringify(server, null, 2)}`);
-        // node.error(JSON.parse(JSON.stringify(server)));
+        node.config = config;
+        node.config.timeout = parseInt(node.config.timeout);
+        node.server = server;
+        // // Further debug code while node is in development
+        // node.error("Node debug:");
+        // node.error(JSON.parse(JSON.stringify(node)));
 
         const options = {
-            // hostname: node.server.host,
-            // port: node.server.port,
-            // path: '/test',
-            timeout: 60000,
-            // protocol: "https:",
+            prefixUrl: node.server.url,
+            timeout: node.config.timeout,
             method: 'GET',
-            maxRedirects: 21,
+            maxRedirects: 3,
+            forever: false,
             headers: {
                 "content-type": "application/json",
                 "X-Requested-With": "TiddlyWiki",
-                "Authorization": "Basic " + new Buffer(server.credentials.username + ':' + server.credentials.password).toString('base64')
+                "Authorization": "Basic " + new Buffer(node.server.credentials.username + ':' + node.server.credentials.password).toString('base64')
             } 
         }
         var nodeContext = this.context();
-        nodeContext.set("test", "value");
-    
+        nodeContext.set("successes", 0);
+        nodeContext.set("failures", 0);
+        nodeContext.set("rate", 0);
 
+        function incsuccesses() {
+            nodeContext.set("successes", parseInt(nodeContext.get("successes"))+1);
+            nodeContext.set("rate", parseInt(nodeContext.get("successes"))/(parseFloat(nodeContext.get("successes")) + parseInt(nodeContext.get("failures"))));
+        }
+        function incfailures() {
+            nodeContext.set("failures", parseInt(nodeContext.get("failures"))+1);
+            nodeContext.set("rate", parseInt(nodeContext.get("successes"))/(parseFloat(nodeContext.get("successes")) + parseInt(nodeContext.get("failures"))));
+        }
+    
         node.search = function(msg, nodeSend, nodeDone) {
             node.trace("Running Tiddlywiki Search")
             var preRequestTimestamp = process.hrtime();
-            node.status({fill:"blue",shape:"dot",text:"httpin.status.requesting"});
 
+            node.status({fill:"blue",shape:"dot",text:"tiddlywiki.status.requesting"});
 
+            // If the input msg has a filter defined allow that to override node filter
             if (msg.filter == undefined) {
-                msg.filter = server.filter;
+                msg.filter = node.server.filter;
             }
 
-            // var filter = encodeURIComponent(msg.filter);
-            var filter = msg.filter;
-            var url = `${server.url}/recipes/default/tiddlers.json?filter=${filter}`;
+            // the filter does not seem to nee encoding
+            var filter = encodeURIComponent(msg.filter);
+            // var filter = msg.filter;
+            var url = `recipes/default/tiddlers.json?filter=${filter}`;
 
             var result = null
             got(url, options).then(res => {
-                node.warn("Success")
+                // node.warn("Success")
                 msg.options = options;
                 msg.statusCode = res.statusCode;
                 // msg.headers = res.headers;
                 msg.responseUrl = res.url;
                 msg.payload = res.body;
-                nodeSend([msg, null]);
-                if (msg.statusCode == 200) {
-                    try { msg.tiddlers = JSON.parse(msg.payload); } // obj
-                    catch(e) { node.warn(RED._("httpin.errors.json-error")); }
-                    msg.tiddlers.map((tid) => {
-                        tid.context = {
-                            wiki_url: server.url,
-                            filter: filter
-                        }
-                        return tid;
-                    });
-                    delete msg.payload;
-                    delete msg.url;
-                    delete msg.statusCode;
-                    // delete msg.topic;
-                    // delete msg.headers;
-                    delete msg.responseUrl;
-                    // delete msg.redirectList;
-                    // delete msg.retry;
-                    var status = { fill: "green", shape: "dot", text: "Success" };
-                    node.status(status);
-                    result = [msg, null];
-                }
-                else {
-                    var status = { fill: "red", shape: "circle", text: "Failed" };
-                    node.status(status);
-                    node.error(`Failed to Get Tiddler: ${msg.responseUrl}, payload: ${msg.payload}`);
-                    result = [null, msg];
-                }
-                //msg.redirectList = redirectList;
-                // Convert the payload to the required return type
-                // if (node.ret !== "bin") {
-                //     msg.payload = msg.payload.toString('utf8'); // txt
 
-                //     if (node.ret === "obj") {
-                //         try { msg.payload = JSON.parse(msg.payload); } // obj
-                //         catch(e) { node.warn(RED._("httpin.errors.json-error")); }
-                //     }
-                // }
                 if (node.metric()) {
                     // Calculate request time
                     var diff = process.hrtime(preRequestTimestamp);
@@ -127,10 +98,71 @@ module.exports = function(RED) {
                         node.metric("size.bytes", msg, res.client.bytesRead);
                     }
                 }
-                //node.status({fill: "green", shape: "dot", text: "Success"});
+
+                msg.requestTime = ms;
+                nodeContext.set("lastrequest", ms);
+
+                if (msg.statusCode == 200) {
+                    var preProcessTimestamp = process.hrtime();
+                    msg.status = "success";
+                    try { msg.tiddlers = JSON.parse(msg.payload); } // obj
+                    catch(e) { node.error(RED._("tiddlywiki.errors.json-error")); }
+                    // Add context to the tiddlers recovered
+                    msg.tiddlers.map((tid) => {
+                        tid.context = {
+                            wiki_url: server.url,
+                            filter: filter
+                        }
+                        return tid;
+                    });
+                    delete msg.payload;
+                    delete msg.url;
+                    // delete msg.statusCode;
+                    // delete msg.topic;
+                    // delete msg.headers;
+                    // delete msg.responseUrl;
+                    // delete msg.redirectList;
+                    // delete msg.retry;
+                    var pdiff = process.hrtime(preProcessTimestamp);
+                    var pms = pdiff[0] * 1e3 + pdiff[1] * 1e-6;
+                    var metricProcessDurationMillis = pms.toFixed(3);
+                    incsuccesses();
+
+                    var status = { fill: "green", shape: "dot", text: `Success (${metricProcessDurationMillis})` };
+                    node.status(status);
+                    result = [msg, null];
+                }
+                else {
+                    incfailures();
+                    msg.status = "failed";
+                    var status = { fill: "red", shape: "circle", text: "Failure" };
+                    node.status(status);
+                    node.error(RED._("tiddlywiki.errors.server_error"));
+                    node.error(`Failed to Get Tiddler: ${msg.responseUrl}, payload: ${msg.payload}`);
+                    result = [null, msg];
+                }
+
+                
+                status.text += " in: " + metricRequestDurationMillis;
+                node.status(status);
                 nodeSend(result);
                 nodeDone();
             }).catch(err => {
+                incfailures();
+                if (node.metric()) {
+                    // Calculate request time
+                    var diff = process.hrtime(preRequestTimestamp);
+                    var ms = diff[0] * 1e3 + diff[1] * 1e-6;
+                    var metricRequestDurationMillis = ms.toFixed(3);
+                    node.metric("duration.millis", msg, metricRequestDurationMillis);
+                    if (res.client && res.client.bytesRead) {
+                        node.metric("size.bytes", msg, res.client.bytesRead);
+                    }
+                }
+
+                msg.requestTime = ms;
+                nodeContext.set("lastrequest", ms);
+
                 if(err.code === 'ETIMEDOUT' || err.code === 'ESOCKETTIMEDOUT') {
                     node.error(RED._("common.notification.errors.no-response"), msg);
                     node.status({fill:"red", shape:"ring", text:"common.notification.errors.no-response"});
@@ -138,9 +170,10 @@ module.exports = function(RED) {
                     node.error(err,msg);
                     node.status({fill:"red", shape:"ring", text:err.code});
                 }
+                msg.options = options;
                 msg.payload = err.toString() + " : " + url;
                 msg.statusCode = err.code || (err.response?err.response.statusCode:undefined);
-                nodeSend(msg);
+                nodeSend([null,msg]);
                 nodeDone();
             });
             
